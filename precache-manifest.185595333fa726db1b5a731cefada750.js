b98550f79c7e3e86296f2163247ceb48017f48aa
mrkjffrsn <mrkjffrsn@users.noreply.github.com>
2020-06-02 21:12:42 +0000

    deploy: bf2176ddb91e3d4b6118320748cca6cf62352be8

---
+++ precache-manifest.185595333fa726db1b5a731cefada750.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e50f13d60a88bcbf3d0d89a665432d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "114c511f3749f1f47f44",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dc1f92bb97f72cb3c6ee",
+    "url": "/static/js/2.42ec3ccd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.42ec3ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "114c511f3749f1f47f44",
+    "url": "/static/js/main.d57acaea.chunk.js"
+  },
+  {
+    "revision": "ecda8e7c18419dbfe4ae",
+    "url": "/static/js/runtime-main.1b2344fd.js"
+  }
+]);
