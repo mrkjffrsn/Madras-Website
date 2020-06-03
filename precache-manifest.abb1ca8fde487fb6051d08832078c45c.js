044990c9e327ff213ed6acd36546425c1f3ac1de
mrkjffrsn <mrkjffrsn@users.noreply.github.com>
2020-06-03 12:53:35 +0000

    deploy: 9d7b2772b384ad92293ce836a24e2717431dc524

---
+++ precache-manifest.abb1ca8fde487fb6051d08832078c45c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa6afef9bff0f5e77589cb3e8f23f6be",
+    "url": "/Madras-Website/index.html"
+  },
+  {
+    "revision": "9f75167627f2372ece28",
+    "url": "/Madras-Website/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ef14f31d85ebcfb528df",
+    "url": "/Madras-Website/static/js/2.08882f29.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Madras-Website/static/js/2.08882f29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f75167627f2372ece28",
+    "url": "/Madras-Website/static/js/main.3191b1d2.chunk.js"
+  },
+  {
+    "revision": "f508f700eff7a69b5614",
+    "url": "/Madras-Website/static/js/runtime-main.cc6ccf84.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dde8e0ebef0db0564878dee3fe60e5a9.js"
+  "/Madras-Website/precache-manifest.abb1ca8fde487fb6051d08832078c45c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Madras-Website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
